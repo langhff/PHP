10d6e4ee13b703f46699bad0e9d4a871a8840953
langhff <langhofffff@gmail.com>
2023-05-03 13:16:39 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Schema::defaultStringLenght(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
